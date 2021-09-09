35fd81f7c3181c8e721cabbb564e4e37fa88ecf8
AleshaMah <amahnukov@gmail.com>
2021-09-09 08:52:31 +0300

    master

--- js_project_all/index.js
+++ js_project_all/index.js
@@ -1,18 +1,12 @@
-let roadMines = [false, false, false, false, true, false, false, true, false, false];
-let step = 2;
+window.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#burger').addEventListener('click', function() {
+        document.querySelector('#menu').classList.toggle('is-active')
+    })
 
-for (let i = 0; i < roadMines.length; i++) {
-    if (roadMines[i] == true) {
-        step -= 1;
-        if (step == 1) {
-            console.log(`Танк переместился на позицию ${i + 1}, танк поврежден`);
-        } else if (step == 0) {
-            console.log(`Танк переместился на позицию ${i + 1}, танк уничтожен`);
-            break;
-        } else {
-            console.log(`Танк переместился на позицию ${i + 1}`);
-        }
-    } else {
-        console.log(`Танк переместился на позицию ${i + 1}`);
-    }
-}
+    document.querySelectorAll ('.text').forEach(function (el){
+        el.addEventListener('click', function(event) {
+        console.log(event)
+        event.target.classList.toggle('alert')
+        })
+    })
+})
